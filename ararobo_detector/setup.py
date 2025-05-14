d53b8c10f4b9d9018cacc8af7169f382b4e49018
Kenshin512 <ketnk2021@gmail.com>
2025-05-14 18:34:08 +0900

    realsenseの画像をリアルタイムで読み取るプログラムを書いた

--- ararobo_detector/setup.py
+++ ararobo_detector/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "test_node = test_node.test_node:main"
+            "test_node = ararobo_detector.test_node:main"
         ],
     },
 )
