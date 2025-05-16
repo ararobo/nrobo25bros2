7b888dc1d5f4c4e3d08b153dfb54cdd01db6d8b2
gn10 <8gn24gn25@gmail.com>
2025-05-16 17:27:54 +0900

    update:cast

--- ararobo_can/src/pc_data_slave.cpp
+++ ararobo_can/src/pc_data_slave.cpp
@@ -39,8 +39,8 @@ bool PCDataSlave::get_odometry(float *x, float *y, float *theta)
         int16_t x_int = static_cast<int16_t>(x_uint);
         int16_t y_int = static_cast<int16_t>(y_uint);
         int16_t theta_int = static_cast<int16_t>(theta_uint);
-        *x = static_cast<float>(x_int) / velocity_scale;
-        *y = static_cast<float>(y_int) / velocity_scale;
+        *x = float(x_int) / velocity_scale;
+        *y = float(y_int) / velocity_scale;
         *theta = static_cast<float>(theta_int) / velocity_scale;
         this->cmd_vel_flag = false;
         return true;
