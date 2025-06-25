e6ae63c1ce5d8610efd930a80fbd2ea2513b9b39
Kenshin512 <ketnk2021@gmail.com>
2025-06-25 18:51:33 +0900

    メッセージ型の変更とその他いろいろ

--- ararobo_core/ararobo_core/core_node.py
+++ ararobo_core/ararobo_core/core_node.py
@@ -1,14 +1,40 @@
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Pose2D
+from std_msgs.msg import Bool
 
 class core_node(Node):
     def __init__(self):
         super().__init__('core_node')
         self.get_logger().info('CoreNode has been initialized.')
         self.box = self.create_subscription(Twist,"/box/box",10)
-        self.box_select = self.create_subscription(Twist,"/box/boxselect",10)
-        self.goal = self.create_publisher(Twist,"/nav/goal",10)
-        self.decbox = self.create_publisher(Twist,"/box/",10)
+        self.box_select = self.create_publisher(Twist,"/box/boxselect",10)
+        self.goal = self.create_publisher(Pose2D,"/nav/goal",10)
+        self.controller = self.create_subscription(Bool,"/controller",10)
+
+        self.box_coller = 0
+        self.team = "red"
+        self.startarea = 0.0
+        self.g2 = 0.0
+        self.g3 = 0.0
+
+    def controller_callback(self, msg):
+        self.get_logger().info(f'Received contrpller message: {msg}')
+        # Process the goal message and publish to nav/goal if needed
+        self.cmd = msg
+        cmds = msg.cmd.split(',')
+        if msg.cmd == "start":
+            msg.data = f"{self.startarea}"
+        elif msg.cmd == "g2":
+            msg.data = f"{self.g2}"
+        elif msg.cmd == "g3":
+            msg.data = f"{self.g3}"
+        elif cmds[0] == "box":
+            if cmds[]
+
+        self.goal.publish(msg)
+
+
 
 def main(args=None):
     rclpy.init(args=args)
