7bf417f36b2e1d16345f0de2201b71917b9e93a5
aiba-gento <8gn24gn25@gmail.com>
2025-07-09 12:12:53 +0900

    add:launch

---
+++ ararobo/launch/navi_launch.py
@@ -0,0 +1,55 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+    # 別のlaunchファイルがあるパッケージの共有ディレクトリを取得
+    ydlidar_share_dir = get_package_share_directory('ydlidar_ros2_driver')
+    slam_share_dir = get_package_share_directory('slam_toolbox')
+
+    # 起動したい子launchファイルのパスを指定
+    ydlidar_launch_file_path = os.path.join(ydlidar_share_dir, 'launch', 'ydlidar_launch.py')
+    slam_launch_file_path = os.path.join(slam_share_dir, 'launch', 'online_async_launch.py')
+
+    return LaunchDescription([
+        Node(
+            package='ararobo_robot',
+            executable='operation_node',
+            name='operation_node',
+            output='screen'
+        ),
+        Node(
+            package='ararobo_robot',
+            executable='feedback_node',
+            name='feedback_node',
+            output='screen'
+        ),
+        Node(
+            package='ararobo_navi',
+            executable='planner_node',
+            name='planner_node',
+            output='screen'
+        ),
+        Node(
+            package='ararobo_control',
+            executable='pure_pursuit_node',
+            name='pure_pursuit_node',
+            output='screen'
+        ),
+        Node(
+            package='ararobo_core',
+            executable='core_node',
+            name='core_node',
+            output='screen'
+        ),
+        # 子となるlaunchファイルをインクルード
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([ydlidar_launch_file_path])
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([slam_launch_file_path])
+        )
+    ])
