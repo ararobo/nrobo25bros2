6bbf0a8f42ca63cbed4c62552b4669b7809c6657
aiba-gento <8gn24gn25@gmail.com>
2025-08-23 10:21:47 +0900

    wip:keyboard

--- ararobo_keyboard/include/ararobo_keyboard/teleop_keyboard.hpp
+++ ararobo_keyboard/include/ararobo_keyboard/teleop_keyboard.hpp
@@ -3,10 +3,25 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <rclcpp/rclcpp.hpp>
 
+class KeyState
+{
+private:
+    struct keystate
+    {
+        bool is_pushed;
+        bool is_long_pushed;
+        uint32_t long_pushed_count;
+    };
+    KeyState::keystate w, a, s, d;
+
+    void update_state(uint8_t code, uint8_t state);
+};
+
 class TeleopKeyboard : public rclcpp::Node
 {
 private:
     std::shared_ptr<KeyboardDriver> keyboard;
+    std::shared_ptr<KeyState> key;
     std::string event_path_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_cmd_;
--- ararobo_keyboard/src/teleop_keyboard.cpp
+++ ararobo_keyboard/src/teleop_keyboard.cpp
@@ -1,9 +1,11 @@
 #include "ararobo_keyboard/teleop_keyboard.hpp"
+#include "ararobo_keyboard/key_code_configure.hpp"
 
 TeleopKeyboard::TeleopKeyboard() : Node("teleop_keyboard")
 {
     keyboard = std::make_shared<KeyboardDriver>();
     keyboard->init(event_path_.c_str());
+    pub_cmd_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
     timer_ = this->create_wall_timer(std::chrono::milliseconds(1), std::bind(&TeleopKeyboard::timer_callback, this));
 }
 
@@ -17,4 +19,5 @@ void TeleopKeyboard::timer_callback()
     geometry_msgs::msg::Twist msg;
     uint8_t key_code, key_state;
     keyboard->get_event(&key_code, &key_state);
+    double x, y, omega = 0;
 }
