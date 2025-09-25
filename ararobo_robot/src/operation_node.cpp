afb293ed23dd89c00c7c74cf74a40054e455042a
aiba-gento <8gn24gn25@gmail.com>
2025-09-26 07:16:48 +0900

    update:timer

--- ararobo_robot/src/operation_node.cpp
+++ ararobo_robot/src/operation_node.cpp
@@ -46,7 +46,7 @@ OperationNode::OperationNode()
         [&](const std_msgs::msg::UInt8::SharedPtr msg) -> void
         { operation_data.comunication_status = msg->data; });
 
-    timer_ = this->create_wall_timer(std::chrono::milliseconds(20),
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(15),
                                      std::bind(&OperationNode::timer_callback, this));
 
     operation_data.comunication_status = 1; // PC-main間通信
