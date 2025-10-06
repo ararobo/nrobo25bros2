52dcd899b1418109403a08c9fd5474653abe9e48
aiba-gento <8gn24gn25@gmail.com>
2025-10-07 07:52:18 +0900

    update:hold speed

--- ararobo_control/include/ararobo_control/hand_node.hpp
+++ ararobo_control/include/ararobo_control/hand_node.hpp
@@ -13,9 +13,9 @@ private:
     float upper_width_speed = 1.0f;
     float under_hand_raise_speed = 1.0f;
     float under_hand_slide_speed = 1.0f;
-    float hold_speed = 25.0f;
+    float hold_speed = 20.0f;
     // 位置制御時速度
-    float upper_depth_pos_speed = 25.0f;
+    float upper_depth_pos_speed = 20.0f;
     float upper_width_pos_speed = 1.0f;
     // 上ハンド
     float upper_depth = 0.0f;
